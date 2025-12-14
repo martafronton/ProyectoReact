2c77c80e5ad04e39fd4638a0d90afd920c40453b
martafronton <martafronton@gmail.com>
2025-12-14 12:37:44 +0100

    feat: primer componente

---
+++ react-proyecto/src/components/infoPeli.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const infoPeli = () => {
+  return (
+    <div style={{ textAlign: "center", margin: "2rem" }}>
+      <h1>🎥 Las películas del año</h1>
+      <p>
+        Bienvenido a la aplicación de películas. Aquí podrás ver información,
+        descubrir el reparto y añadir tus favoritas.
+      </p>
+    </div>
+  );
+};
+
+export default infoPeli;
